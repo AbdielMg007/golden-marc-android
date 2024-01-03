107e921c0536512a07a0687dd63191a35cc3a6b3
abdielmg007 <abdiel.mugo@gmail.com>
2024-01-02 23:44:13 -0600

    ADD fragments for the login structure

---
+++ app/src/main/java/com/mg/goldenmarc/ui/view/activities/MenuActivity.kt
@@ -0,0 +1,12 @@
+package com.mg.goldenmarc.ui.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mg.goldenmarc.R
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
